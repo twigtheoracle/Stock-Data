4953cce0308d70cf6e697f7def9b976e2716025c
Eric Liu <ericliu121@yahoo.com>
2017-07-07 18:00:32 -0700

    getMonthPercentageChange works!
    getMonthPercentageChange works and properly returns the percentage
    change for the given year, given month, and given stock :D

--- generate_data.py
+++ generate_data.py
@@ -5,11 +5,16 @@ from openpyxl.styles import Alignment
 import quandl
 import random
 import string
+import calendar
+import datetime
 
 ################################################################################
 
 # data :)
 monthIndex = ["Jan (1)", "Feb (2)", "Mar (3)", "Apr (4)", "May (5)", "Jun (6)", "Jul (7)", "Aug (8)", "Sep (9)", "Oct (10)", "Nov (11)", "Dec (12)"]
+
+# other import thing(s) :)
+quandl.ApiConfig.api_key = "1qsGVmxih-dcMRsh13Zk"
 
 ################################################################################
 
@@ -22,13 +27,17 @@ def formatDate(dateString):
 
 ################################################################################
 
+def formatQuandlQuery(stock, startDate, endDate):
+	return ("WIKI/PRICES.json?date.gte=" + startDate + "&date.lt=" + endDate + "&ticker=" + stock + "&api_key=1qsGVmxih-dcMRsh13Zk")
+
+################################################################################
+
 def getHistoricalData(stockName, dateOld, dateCurrent):
 
 	dateOldFormatted = formatDate(str(dateOld))
 	dateCurrentFormatted = formatDate(str(dateCurrent))
 
-	quandl.ApiConfig.api_key = "1qsGVmxih-dcMRsh13Zk"
-	data = quandl.get_table("WIKI/PRICES.json?date.gte=" + dateOldFormatted + "&date.lt=" + dateCurrentFormatted + "&ticker=" + stockName + "&api_key=1qsGVmxih-dcMRsh13Zk")
+	data = quandl.get_table(formatQuandlQuery(stockName, dateOldFormatted, dateCurrentFormatted))
 
 	length = len(data["date"])
 
@@ -84,8 +93,83 @@ def formatPercOrFreqSheet(sheet, currentYear, currentMonth):
 
 ################################################################################
 
-def fillPercAndFreq(percSheet, freqSheet, thisYear, thisMonth, rowNumber, stockName):
-	percSheet["A" + str(rowNumber)] = stockName
-	freqSheet["A" + str(rowNumber)] = stockName
-	
+def nextMonth(month):
+	if (month == 12):
+		return 1
+	return month + 1
 
+################################################################################
+
+def nextYear(year, month):
+	if (month == 12):
+		return year + 1
+	return year
+
+################################################################################
+
+def firstValidDate(year, month):
+	deltaT = datetime.timedelta(days = 1)
+	date = datetime.date(year, month, 1)
+	while True:
+		if (date.weekday() > 4):
+			date = date + deltaT
+		else:
+			break
+
+	firstDate = str(date)
+	secondDate = str(date + deltaT)
+
+	return [firstDate, secondDate]
+
+################################################################################
+
+def lastValidDate(year, month):
+	deltaT = datetime.timedelta(days = 1)
+	date = datetime.date(year, month, calendar.monthrange(year, month)[1])
+	while True:
+		if (date.weekday() > 4):
+			date = date - deltaT
+		else:
+			break
+
+	firstDate = str(date)
+	secondDate = str(date + deltaT)
+
+	return [firstDate, secondDate]
+
+################################################################################
+
+def getMonthPercentageChange(stock, year, month):
+	print(stock)
+	for i in range(year, year - 11, -1):
+		firstDays = firstValidDate(i, month)
+		lastDays = lastValidDate(i, month)
+
+		secondFirstDate = str(i) + "-" + str(month) + "-2"
+		secondLastDate = str(nextYear(i, month)) + "-" + str(nextMonth(month)) + "-" + "1"
+
+		firstDayData = quandl.get_table(formatQuandlQuery(stock, firstDays[0], firstDays[1]))
+		lastDayData = quandl.get_table(formatQuandlQuery(stock, lastDays[0], lastDays[1]))
+
+		# print(firstDayData, lastDayData)
+
+		firstDayClosePrice = firstDayData["close"][0]
+		lastDayClosePrice = lastDayData["close"][0]
+
+		percentChange = ((lastDayClosePrice - firstDayClosePrice)/firstDayClosePrice) * 100
+
+		print(str(i) + " " + str(month) + " percentage change = " + str(percentChange))
+	
+	print()
+
+
+
+################################################################################
+
+def fillPercAndFreq(percSheet, freqSheet, thisYear, thisMonth, rowNumber, stock):
+	percSheet["A" + str(rowNumber)] = stock
+	freqSheet["A" + str(rowNumber)] = stock
+
+	getMonthPercentageChange(stock, thisYear, thisMonth - 1)
+	
+################################################################################
