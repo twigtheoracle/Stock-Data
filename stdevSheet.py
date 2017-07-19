e3de52d5589e64868c822f118967d61e3120b7b1
Eric Liu <ericliu121@yahoo.com>
2017-07-18 21:16:11 -0700

    stdev fill and color
    ^^^

--- stdevSheet.py
+++ stdevSheet.py
@@ -83,3 +83,31 @@ class stdevSheet():
 
 		for row in range(3, 3 + len(self.stockList)):
 			self.sheet["A" + str(row)] = self.stockList[row - 3]
+
+	# fills the frequency sheet with cool data
+	def fill(self):
+		index = 0
+		for row in range(3, 3 + len(self.stockList)):
+			for letter in range(2, 15): # from letter B to N when plugged into the first non __init__ function
+				cell = self.numberToLetter(letter) + str(row)
+
+				if (self.sheet[self.numberToLetter(letter) + "2"].value != None):
+
+					self.sheet[cell] = self.stdevList[index]
+
+					index += 1
+
+	# colors the stdev sheet on a red green gradient
+	def color(self):
+		for letter in range(2, 15): # from letter B to N when plugged into the first non __init__ function
+			for row in range(3, 3 + len(self.stockList)):
+				cell = self.numberToLetter(letter) + str(row)
+				value = self.sheet[cell].value
+
+				if (value != None):
+					value = 20-int(value*2)
+					if (value <= 0):
+						value = 0
+					elif (value >= 19):
+						value = 19
+					self.sheet[cell].fill = self.colorGradient[value]
