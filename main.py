7bd9ccbd0dab1b74d94719f218f69c91bd08f02a
Eric Liu <ericliu121@yahoo.com>
2017-07-18 21:15:48 -0700

    try except wrapper
    to catch the 504 error that sometimes appears

--- main.py
+++ main.py
@@ -1,76 +1,89 @@
 from pprint import pprint
 
+import openpyxl
+import time
+
 import functions as f
 import stock as s
 import percentageSheet as ps
 import frequencySheet as fs
+import stdevSheet as sds
 
-import openpyxl
-import time
+try:
+	start = time.time()
 
-start = time.time()
+	fileName = "test_template.xlsx"
+	bins = 20
+	savePath = "C:/Users/ericl/Desktop/"
 
-fileName = "template.xlsx"
-bins = 20
-savePath = "C:/Users/ericl/Desktop/"
+	wb = openpyxl.load_workbook(fileName)
 
-wb = openpyxl.load_workbook(fileName)
+	run = f.fileOpen(wb, fileName, savePath)
 
-run = f.fileOpen(wb, fileName, savePath)
+	if(run):
 
-if(run):
+		# recent data
+		for sheet in wb:
 
-	# recent data
-	for sheet in wb:
+			print(sheet.title + " ANALYSIS")
 
-		print(sheet.title + " ANALYSIS")
+			stock = s.stock(sheet.title, sheet, bins, savePath)
+			stock.formatRecentDataSheet()
+			hd = stock.getHistoricalData()
+			prices = stock.fillRecentData(hd)
+			stock.fillRecentDescriptiveStats(prices)
+			stock.fillRecentGraphs(prices)
 
-		stock = s.stock(sheet.title, sheet, bins, savePath)
-		stock.formatRecentDataSheet()
-		hd = stock.getHistoricalData()
-		prices = stock.fillRecentData(hd)
-		stock.fillRecentDescriptiveStats(prices)
-		stock.fillRecentGraphs(prices)
+			print("COMPLETED\n")
 
-		print("COMPLETED\n")
+		# 10 year stuff
+		# percentage sheet
+		stockList = wb.sheetnames
+		foo = wb.create_sheet("10YR %", 0)
+		percentageSheet = ps.percentageSheet(foo, stockList)
 
-	# 10 year stuff
-	# percentage sheet
-	stockList = wb.sheetnames
-	foo = wb.create_sheet("10YR %", 0)
-	percentageSheet = ps.percentageSheet(foo, stockList)
+		frequencyList = []
+		stdDevList = []
 
-	frequencyList = []
-	stdDevList = []
+		percentageSheet.format()
+		for i in range(0, len(stockList)):
 
-	percentageSheet.format()
-	for i in range(0, len(stockList)):
+			percentageSheet.addStock(i + 3, stockList[i])
 
-		percentageSheet.addStock(i + 3, stockList[i])
+			for monthOffset in range(0,12):
 
-		for monthOffset in range(0,12):
+				bar, bat = percentageSheet.fillPercentageChange(stockList[i], monthOffset, i)
+				frequencyList.append(bar)
+				stdDevList.append(bat)
 
-			bar, bat = percentageSheet.fillPercentageChange(stockList[i], monthOffset, i)
-			frequencyList.append(bar)
-			stdDevList.append(bat)
-
-	percentageSheet.color()
-
-	# frequency sheet
-	foo = wb.create_sheet("10YR FREQ", 1)
-
-	frequencySheet = fs.frequencySheet(foo, frequencyList, stockList)
-	frequencySheet.format()
-	frequencySheet.fill()
-	frequencySheet.color()
+		percentageSheet.color()
 
 
-	# standard deviation sheet
+		# standard deviation sheet
+		foo = wb.create_sheet("10YR % STD Dev", 1)
+
+		stdevSheet = sds.stdevSheet(foo, stdDevList, stockList)
+		stdevSheet.format()
+		stdevSheet.fill()
+		stdevSheet.color()
 
 
-	#save workbook
-	if (f.save(wb, savePath)):
-		print("WORKBOOK COMPLETED")
+		# frequency sheet
+		foo = wb.create_sheet("10YR FREQ", 2)
 
-timeElapsed = time.time() - start
-print("\nELAPSED TIME: ", int(int(timeElapsed)/60), int(timeElapsed)%60)
+		frequencySheet = fs.frequencySheet(foo, frequencyList, stockList)
+		frequencySheet.format()
+		frequencySheet.fill()
+		frequencySheet.color()
+
+		#save workbook
+		if (f.save(wb, savePath)):
+			print("WORKBOOK COMPLETED")
+
+	timeElapsed = time.time() - start
+	# im not sure if time will automatically cast to an int and im too lazy to look it up :D
+	print("\nELAPSED TIME:", str(int(int(timeElapsed)/60)) + "m", str(int(timeElapsed)%60)+"s")
+
+except quandl.errors.quandl_error.QuandlError:
+	print("504: GATEWAY TIMEOUT ERROR")
+	print("TRY TO RUN THE PROGRAM AGAIN")
