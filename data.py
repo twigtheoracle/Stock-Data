4ac95fac293b5a6d9c01086c4a9e13c510d1b621
Eric Liu <ericliu121@yahoo.com>
2017-09-07 19:06:51 -0700

    data class

---
+++ data.py
@@ -0,0 +1,27 @@
+import datetime
+
+class Data():
+    # initializes data with the list of stocks and proper dates
+    def __init__(self, stock_list):
+        self.stock_list = stock_list
+        current_date = str(datetime.date.today())
+        #TODO: verify that old_date will exist (think leap years)
+        old_date = str(int(current_date[:4]) - 10) + current_date[4:]
+        self.current_date = current_date
+        self.old_date = self.veriy_date(old_date)
+
+    def veriy_date(self, date):
+        pass
+
+    # returns stock_list as a string of the stocks delimited by commas
+    def get_stock_string(self):
+        return_string = ""
+        for index in range(0, len(self.stock_list)):
+            return_string += self.stock_list[index]
+            if(index != len(self.stock_list) - 1):
+                return_string += ","
+        return return_string
+
+    def get_quandl_query_string(self):
+		returnString = ("WIKI/PRICES.json?date.gte=" + self.old_date + "&date.lt=" + self.current_date + "&ticker=" + self.get_stock_string + "&api_key=" + key.getAPIKey())
+		return returnString
