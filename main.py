82cccc8b7c02b4e222e7b2b56cc6780e420977dc
Eric Liu <twigoracle@gmail.com>
2020-12-25 12:18:49 -0800

    Changed file structure

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# Eric Liu
+
+# TODO
+
 from pprint import pprint
 from tqdm import tqdm
 
@@ -5,83 +9,116 @@ from tqdm import tqdm
 import ssl
 import requests
 
+import argparse
 import openpyxl
 import time
 import quandl
+import json
 
-from functions import *
-from stock import *
-from data import *
-from sheet import *
+from src.functions import *
 
 ########################################################################################
 
 def main():
 
-    start = time.time()
+    # read and process command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config", type=str, nargs=1, metavar="str", 
+        default="config/default.json", help="The path to the desired config file. By default " \
+        "this takes the value of \"config/default.json\"")
+    parser.add_argument("--test", action="store_true",
+        help="When present, the program will override the \"tickers\" parameter of the config " \
+        "file to only process on AAPL and ZTE. This will also override the \"--tickers\" argument")
+    parser.add_argument("--tickers", type=str, metavar="str", default=None,
+        help="Define tickers using a file instead of the config file. This will overwrite the " \
+        "tickers included in the config file. The file must contain one ticker per line. See " \
+        "TODO for an example file")
+    args = parser.parse_args()
 
-    try:
-        save_path = get_save_path()
+    # open and process the config file
+    with open(get_path(args.config), "r") as f:
+        config = json.load(f)
+    
+        # ensure that the save location exists and is a directory
+        check_directory(config["save_location"])
 
-        # get the workbook and stock list
-        # depends on the file format of the template
-        template_file = "./templates/template 20-06-2020.txt"
-        wb, stock_list = get_workbook_and_stocklist(template_file)
+        # change the paths to absolute
+        config["save_location"] = get_path(config["save_location"])
 
-        data = Data(stock_list)
-        data.retrieve_data(data_provider = "Quandl")
-        short_term_data = data.get_short_term_data()
+        # change tickers if the "--test" flag is present
+        if(args.test):
+            config["tickers"] = ["AAPL", "ZTE"]
 
-        # print(data.data)
-        # print(short_term_data)
+    # get the workbook formatted with the input tickers
+    wb = get_workbook(config["tickers"])
 
-        print("\ncreating stock sheets...")
-        for sheet in wb:
-            stock_sheet = Stock(sheet, short_term_data[sheet.title])
-            stock_sheet.format()
-            stock_sheet.fill_data()
-            stock_sheet.fill_stats()
-            stock_sheet.fill_graphs()
-        print("done")
+    # save the wb
+    save(wb, config["save_location"])
+
+    # start = time.time()
+
+    # try:
+    #     save_path = get_save_path()
+
+    #     # get the workbook and stock list
+    #     # depends on the file format of the template
+    #     template_file = "./templates/template 20-06-2020.txt"
+    #     wb, stock_list = get_workbook_and_stocklist(template_file)
+
+    #     data = Data(stock_list)
+    #     data.retrieve_data(data_provider = "Quandl")
+    #     short_term_data = data.get_short_term_data()
+
+    #     # print(data.data)
+    #     # print(short_term_data)
+
+    #     print("\ncreating stock sheets...")
+    #     for sheet in wb:
+    #         stock_sheet = Stock(sheet, short_term_data[sheet.title])
+    #         stock_sheet.format()
+    #         stock_sheet.fill_data()
+    #         stock_sheet.fill_stats()
+    #         stock_sheet.fill_graphs()
+    #     print("done")
         
-        long_term_data = data.get_long_term_data()
+    #     long_term_data = data.get_long_term_data()
 
-        # print(long_term_data)
+    #     # print(long_term_data)
 
-        temp_sheet = wb.create_sheet("10YR %", 0)
-        percentage_sheet = Sheet(temp_sheet, long_term_data["percent_change"], stock_list, long_term_data["years"])
-        percentage_sheet.format()
-        percentage_sheet.fill()
-        percentage_sheet.color(-10, 10)
+    #     temp_sheet = wb.create_sheet("10YR %", 0)
+    #     percentage_sheet = Sheet(temp_sheet, long_term_data["percent_change"], stock_list, long_term_data["years"])
+    #     percentage_sheet.format()
+    #     percentage_sheet.fill()
+    #     percentage_sheet.color(-10, 10)
 
-        temp_sheet = wb.create_sheet("10YR % STD DEV", 1)
-        # note: std dev here is std dev of percentage change, not of average value of the month
-        std_dev_sheet = Sheet(temp_sheet, long_term_data["std_dev"], stock_list, long_term_data["years"])
-        std_dev_sheet.format()
-        std_dev_sheet.fill()
-        # TODO: do some work to figure out optimal numbers for std dev coloring
-        std_dev_sheet.color(.1, .02)
+    #     temp_sheet = wb.create_sheet("10YR % STD DEV", 1)
+    #     # note: std dev here is std dev of percentage change, not of average value of the month
+    #     std_dev_sheet = Sheet(temp_sheet, long_term_data["std_dev"], stock_list, long_term_data["years"])
+    #     std_dev_sheet.format()
+    #     std_dev_sheet.fill()
+    #     # TODO: do some work to figure out optimal numbers for std dev coloring
+    #     std_dev_sheet.color(.1, .02)
 
-        temp_sheet = wb.create_sheet("10YR FREQ", 2)
-        freq_sheet = Sheet(temp_sheet, long_term_data["freq"], stock_list, long_term_data["years"])
-        freq_sheet.format()
-        freq_sheet.fill()
-        freq_sheet.color(20, 80)
+    #     temp_sheet = wb.create_sheet("10YR FREQ", 2)
+    #     freq_sheet = Sheet(temp_sheet, long_term_data["freq"], stock_list, long_term_data["years"])
+    #     freq_sheet.format()
+    #     freq_sheet.fill()
+    #     freq_sheet.color(20, 80)
 
-    # this doesn't work to stop no wifi errors for some reason
-    # TODO: catch no wifi error
-    except ConnectionError:
-        print("\nERROR: NO INTERNET")
-    except requests.exceptions.SSLError:
-        print("\nERROR: SSL Certificate is not valid")
-    except quandl.errors.quandl_error.QuandlError as err:
-        print(err)
+    # # this doesn't work to stop no wifi errors for some reason
+    # # TODO: catch no wifi error
+    # except ConnectionError:
+    #     print("\nERROR: NO INTERNET")
+    # except requests.exceptions.SSLError:
+    #     print("\nERROR: SSL Certificate is not valid")
+    # except quandl.errors.quandl_error.QuandlError as err:
+    #     print(err)
         
-    time_elapsed = time.time() - start
-    print()
-    print("time elapsed: " + str(int(int(time_elapsed) / 60)) + "m " + str(int(time_elapsed) % 60) + "s")
+    # time_elapsed = time.time() - start
+    # print()
+    # print("time elapsed: " + str(int(int(time_elapsed) / 60)) + "m " + str(int(time_elapsed) % 60) + "s")
 
-    save(wb, save_path)
+    # save(wb, save_path)
 
 if __name__ == '__main__':
     main()
---
+++ src/functions.py
@@ -0,0 +1,94 @@
+# Eric Liu
+
+# TODO
+
+import openpyxl
+import datetime
+import os
+
+def get_path(relative_path):
+    """
+    Returns an absolute path from the root of the project directory based on the input relative
+    path
+
+    :param:     relative_path   The relative path to convert to an absolute path
+    :return:    str             The relative path as an absolute path
+    """
+    # get the absolute path
+    abs_path = os.path.abspath(relative_path)
+
+    # for some reason, if the input path is a directory, then the trailing "\" is removed
+    if(os.path.isdir(abs_path)):
+        abs_path += "\\"
+
+    # return the path
+    return abs_path
+
+def check_directory(path):
+    """
+    Check if the input path (relative or absolute) exists and is a directory. If the input path
+    is relative, it is assumed to be relative to the root of the project directory. If the path
+    does not exist, it is created. If the path exists as a file, an error is thrown. If the path 
+    exists as a directory, nothing is done
+
+    :param:     path    The path to check 
+    """
+    # convert to absolute path
+    path = get_path(path)
+
+    # check that the path exists and is not a directory
+    if(os.path.exists(path) and not os.path.isdir(path)):
+        # raise an error indicitating that the path exists as a file and not a directory
+        raise FileExistsError("Save path \"" + path + 
+            "\" exists as a file and not a directory already")
+    # check that the path does not exist
+    elif(not os.path.exists(path)):
+        # create the directory
+        os.mkdir(path)
+    # check that the path exists and is a directory
+    elif(os.path.exists(path) and os.path.isdir(path)):
+        # do nothing
+        pass
+
+def get_workbook(tickers):
+    """
+    Create an xl workbook using openpyxl and create the sheets in the workbook based on the input
+    tickers
+
+    :param:     tickers     A list containing all tickers
+
+    :return:    Workbook    A Workbook created by openpyxl containing a sheet for every stock
+    """
+    # create the Workbook
+    wb = openpyxl.Workbook()
+
+    # add each ticker as a sheet in the wb
+    for i in range(0, len(tickers)):
+        # remove whitespace, tabs, and newlines
+        sheet_title = "".join(tickers[i].split())
+
+        # add the sheet to the wb
+        wb.create_sheet(sheet_title, i + 1)
+
+    # remove the original default sheet
+    wb.remove_sheet(wb.get_sheet_by_name("Sheet"))
+
+    # return the wb
+    return wb
+
+def save(wb, save_location):
+    """
+    Save the input workbook at the specified location. This saves using the filename:
+    "option_analysis_{today's date}.xlsx"
+
+    :param:     wb              The workbook to save
+    :param:     save_location   The location at which to save. This should be a directory, and not
+                                a file
+    """
+    try:
+        # attempt to save at the requested location
+        wb.save(save_location + "option_analysis_" + str(datetime.date.today()) + ".xlsx")
+        print(save_location + "option_analysis_" + str(datetime.date.today()) + ".xlsx")
+    except PermissionError:
+        # raise an error if the file is currently open
+        raise PermissionError("File cannot be saved since it is open. Close the file and run again")
