8ed75370202346e320161ac2495798f12f9e572a
Eric Liu <twigoracle@gmail.com>
2020-12-25 12:21:06 -0800

    Added empty api key function

---
+++ src/api_key.py
@@ -0,0 +1,13 @@
+# Eric Liu
+# 
+# This file contains the function get_API_key(), which returns the Quandl API key. The pushed 
+# version of this function returns and empty string. This function should be modified to return
+# the actual API key before running the project
+
+def get_API_key():
+    """
+    Return the Quandl API key
+
+    :return:    str     The Quandl API key. This file should never be pushed to the repo
+    """
+    return ""
